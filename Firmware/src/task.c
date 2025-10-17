12164d9827fbe3fd5e372cc313ced98cc534c136
aKaReZa <aKaReZa75@gmail.com>
2025-10-17 12:20:41 +0330

    add display Task

--- Firmware/src/task.c
+++ Firmware/src/task.c
@@ -1,7 +1,16 @@
 #include "task.h"
 
+extern volatile uint32_t System_millis;
+
+millis_T millisDisplay = {.Delta = 0, .Previous = 0, .Interval = 250};
 
 void scheduler(void)
 {
+    millisDisplay.Delta = System_millis - millisDisplay.Previous;
 
+    if(millisDisplay.Delta > millisDisplay.Interval)
+    {
+        millisDisplay.Previous = System_millis;
+        display_Update();
+    };
 };
