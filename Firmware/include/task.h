c9a9a12a2febedc7fc150881967c9994b17a6e87
aKaReZa <aKaReZa75@gmail.com>
2025-10-17 12:46:38 +0330

    add gsm task

--- Firmware/include/task.h
+++ Firmware/include/task.h
@@ -3,13 +3,30 @@
 
 #include "aKaReZa.h"
 
+
 typedef enum
 {
     Sys_StartUp = 0,
     Sys_Idle
 } Sys_State_T;
 
+
+typedef enum
+{
+	GSM_Reset = 0,
+	GSM_StartUp,
+	GSM_Init,
+	GSM_Idle,
+    GSM_SMS_ParseNumber,    
+    GSM_SMS_ReadContent,    
+    GSM_SMS_SendSMS,        
+	GSM_Fault
+} GSM_State_T;
+
+
+
 void scheduler(void);
 void System_Task(void);
+void GSM_Task(void);
 
 #endif
--- Firmware/src/task.c
+++ Firmware/src/task.c
@@ -6,14 +6,25 @@ extern volatile uint32_t System_millis;
 millis_T millisDisplay = {.Delta = 0, .Previous = 0, .Interval = 250};
 millis_T millisOutputs = {.Delta = 0, .Previous = 0, .Interval = 50};
 millis_T millisSystem  = {.Delta = 0, .Previous = 0, .Interval = 50};
+millis_T millisGSM     = {.Delta = 0, .Previous = 0, .Interval = 100};
+
 
 Sys_State_T Sys_State = Sys_StartUp;
+GSM_State_T GSM_State = GSM_Reset;
+
 
 void scheduler(void)
 {
     millisDisplay.Delta = System_millis - millisDisplay.Previous;
     millisOutputs.Delta = System_millis - millisOutputs.Previous;
     millisSystem.Delta  = System_millis - millisSystem.Previous;
+    millisGSM.Delta     = System_millis - millisGSM.Previous;
+
+    if(millisGSM.Delta > millisGSM.Interval)
+    {
+        millisGSM.Previous = System_millis;
+        GSM_Task();
+    };
 
     if(millisSystem.Delta > millisSystem.Interval)
     {
@@ -50,5 +61,44 @@ void System_Task(void)
         case Sys_Idle:
 
         break;
+    };
+};
+
+
+void GSM_Task(void)
+{
+    switch (GSM_State)
+    {
+        case GSM_Reset:
+
+        break;
+    
+        case GSM_StartUp:
+
+        break;
+
+        case GSM_Init:
+
+        break;   
+
+        case GSM_Idle:
+
+        break;  
+        
+        case GSM_SMS_ParseNumber:
+
+        break;   
+        
+        case GSM_SMS_ReadContent:
+
+        break; 
+        
+        case GSM_SMS_SendSMS:
+
+        break;   
+        
+        case GSM_Fault:
+
+        break;          
     }
 };
